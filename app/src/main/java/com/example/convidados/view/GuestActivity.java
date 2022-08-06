d852c5c45542af3dc1cef3ec600a5381b3af46da
israel Nascimento <isra.nascimento@gmail.com>
2022-08-06 12:09:29 -0300

    Cadastro - Repositório

--- app/src/main/java/com/example/convidados/view/GuestActivity.java
+++ app/src/main/java/com/example/convidados/view/GuestActivity.java
@@ -1,6 +1,7 @@
 package com.example.convidados.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,15 +10,21 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 
 import com.example.convidados.R;
+import com.example.convidados.constants.GuestConstants;
+import com.example.convidados.model.GuestModel;
+import com.example.convidados.viewmodel.GuestViewModel;
 
-public class GuestActivity extends AppCompatActivity {
+public class GuestActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ViewHolder mViewHolder = new ViewHolder();
+    private GuestViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_form);
+
+        this.mViewModel = new ViewModelProvider(this).get(GuestViewModel.class);
 
         this.mViewHolder.editName = findViewById(R.id.edit_name);
         this.mViewHolder.radioNotConfirmed = findViewById(R.id.radio_not_confirmed);
@@ -28,13 +35,32 @@ public class GuestActivity extends AppCompatActivity {
         this.setListeners();
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_save){
+            this.handleSave();
+        }
+    }
+
     private void setListeners() {
-        this.mViewHolder.buttonSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO
-            }
-        });
+        this.mViewHolder.buttonSave.setOnClickListener(this);
+    }
+
+    private void handleSave(){
+        String name = this.mViewHolder.editName.getText().toString();
+
+        int confirmation = 0;
+
+        if(this.mViewHolder.radioPresent.isChecked() ){
+            confirmation = GuestConstants.CONFIRMATION.PRESENT;
+        }else if (this.mViewHolder.radioAbsent.isChecked()){
+            confirmation = GuestConstants.CONFIRMATION.ABSENT;
+        }
+
+        GuestModel guest = new GuestModel(name, confirmation);
+
+        this.mViewModel.save(guest);
+
     }
 
     private static class ViewHolder{
